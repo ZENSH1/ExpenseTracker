738cb3f33910b88896fa38b8ff4389f89370658d
Syed Zain ul Abidin Sherazi <95184377+ZENSH1@users.noreply.github.com>
2026-02-21 01:56:34 +0500

    feat: Add copy-to-clipboard toast notification
    This commit introduces a user-facing improvement by displaying a "Copied to clipboard" toast notification whenever a user long-presses an `InfoRow` component to copy its value.
    
    Key changes include:
    - A new `showToast` utility function has been added to `Utils.kt` to centralize toast notifications.
    - The `InfoRow` composable now uses this utility to provide immediate feedback to the user upon a successful copy action.

--- app/src/main/java/com/xs/expensetracker/utils/Utils.kt
+++ app/src/main/java/com/xs/expensetracker/utils/Utils.kt
@@ -1,10 +1,16 @@
 package com.xs.expensetracker.utils
 
+import android.content.Context
 import android.util.Log
+import android.widget.Toast
 
 object Utils {
     fun Any.log(){
         Log.d("ExpenseTracker", this.toString())
     }
+
+    fun showToast(context: Context, message: String) {
+        Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
+        }
 
 }
