1d8a230285ea4ba9cd5df57423f2f59b2759b446
Syed Zain ul Abidin Sherazi <95184377+ZENSH1@users.noreply.github.com>
2026-02-22 01:06:04 +0500

    build: Enable minification and update app version
    This commit updates the `app/build.gradle.kts` file to prepare for release builds.
    
    It enables code shrinking (`isMinifyEnabled = true`) and resource shrinking (`isShrinkResources = true`) for the `release` build type to optimize the application size.
    
    Additionally, the application version is updated:
    - `versionCode` is incremented from 1 to 3.
    - `versionName` is changed from "1.0" to "0.0.3".

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,15 +18,16 @@ android {
         applicationId = "com.xs.expensetracker"
         minSdk = 24
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 3
+        versionName = "0.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
     buildTypes {
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
+            isShrinkResources = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
